e46ccd6cbd383c9a780bf20aa37748dad1e05230
hamedaskarian <43287185+hamedaskarian@users.noreply.github.com>
2019-02-05 10:04:31 +0330

    Add project files.

---
+++ DourtalAdoDatabase01/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DourtalAdoDatabase01 
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Models.DatabaseContext oDatabaseContext = null;
+            try
+            {
+                oDatabaseContext = new Models.DatabaseContext();
+
+                Models.Person oPeson = new Models.Person();
+
+                oPeson.Id = 5;
+                oPeson.Age = 32;
+                oPeson.FullName = "hamed";
+                oPeson.IsSupervisor = true;
+
+                oDatabaseContext.People.Add(oPeson);
+                oDatabaseContext.SaveChanges();
+           
+
+            }
+            catch (System.Exception ex)
+            {
+
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (oDatabaseContext != null)
+                {
+                    oDatabaseContext.Dispose();
+                    oDatabaseContext = null;
+
+                }
+            }
+        }
+
+        
+
+    }
+     
+    
+}
---
+++ DourtalAdoDatabase01/Models/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class DatabaseContext : System.Data.Entity.DbContext
+    {
+        public DatabaseContext() //constuctor
+        {
+               
+        }
+
+        public System.Data.Entity.DbSet<Person> People { get; set; }
+    }
+}
